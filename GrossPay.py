bf5112c7b9caecb4c72012d4056df274791954ea
Evelyn <77958012+evarito@users.noreply.github.com>
2023-09-14 17:35:58 +0100

    First exercise commit
    This is a code that calculates gross pay by prompting the user to enter the number of hours worked and the rate per hour, then displays the gross pay.

---
+++ GrossPay.py
@@ -0,0 +1,11 @@
+# prompt user for hours and ratae per hour
+hours_worked = int(input("Enter Hours_worked: "))
+rate_per_hour = float(input("Enter Rate_per_hour: "))
+
+#calculating the gross pay 
+Gross_pay = hours_worked*rate_per_hour
+
+#print the gross pay
+print("THE GROSS_PAY IS:  ", Gross_pay)
+
+ 
