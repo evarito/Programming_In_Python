c5a51e025f61673fe9973a72346394826b1eeb1e
Evelyn <77958012+evarito@users.noreply.github.com>
2023-09-24 13:08:22 +0100

    third commit
    the code tests whether a number is divisible by 11 using a number provided by the user

---
+++ Divisibility_Test.py
@@ -0,0 +1,6 @@
+#Testing the divisibility of a number by 11
+num = int(input("Enter a number:  "))
+if num % 11 == 0:
+    print("The Number is Divisible by 11")
+else:
+    print("The Number is not Divisible by 11")
