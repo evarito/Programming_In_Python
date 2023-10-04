d2777d84845f4a737df690928b99a13a6086edd8
Evelyn <77958012+evarito@users.noreply.github.com>
2023-10-04 12:39:02 +0100

    eighth commit
    The program determines the maximum and minimum numbers in a heterogeneous list, making use of the max() and min() functions.

---
+++ Max&Min_list.py
@@ -0,0 +1,9 @@
+# the program finds the maximum and minimum values in a heterogenous list
+het_list = [45, 78.98, -6700, -987]
+
+maximum_value = max(het_list)
+minimum_value = min(het_list)
+
+print("The maximum value is: ", maximum_value)
+print("The minimum value is: ", minimum_value)
+
