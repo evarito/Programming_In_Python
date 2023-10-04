c53999efbe4b2d2e6843dd5c87c2183a1204640e
Evelyn <77958012+evarito@users.noreply.github.com>
2023-10-04 12:58:09 +0100

    ninth commit
    The program concatenates different dictionaries to form one new dictionary, using the update function.

---
+++ Merging_Dictionaries.py
@@ -0,0 +1,9 @@
+# A program that merges different dictionanries to form one new dictonary 
+
+dict_a = {'F_name': "Eve", 'L_name': "Lubanga"}
+dict_b = {'School': "WoodCreek", 'Staff_No': 154889}
+dict_c = {'Department': "ICT", 'Supervisor': None}
+dict_d = {}
+
+for d in (dict_a,dict_b,dict_c): dict_d.update(d)
+print(dict_d)
